ec308264bf4934e49a8d332a227b1122e2d73577
cyrilnoah1 <cyril.noah1@gmail.com>
2017-04-06 15:23:36 +0530

    feat: Added login service helper.

--- app/src/main/java/com/example/rapid/telematics/server/ServiceHelper.java
+++ app/src/main/java/com/example/rapid/telematics/server/ServiceHelper.java
@@ -1,10 +1,23 @@
 package com.example.rapid.telematics.server;
 
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 /**
  * Service helper containing the required endpoint for accessing data.
  */
 public interface ServiceHelper {
- //TODO: Add the GET and POST retro methods here.
+
+    // LOGIN:
+    @FormUrlEncoded
+    @POST("authz/oauth2/token")
+    Call<JsonObject> login(@Field("username") String username, @Field("password") String password,
+                           @Field("grant_type") String grantType, @Field("client_id") String clientId);
+    
 }
 
 
